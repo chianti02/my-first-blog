30567e8f6f142bb537301bf2faa6bd928581ac70
Kathy Chou <kchou020281@gmail.com>
2016-08-10 00:12:26 -0700

    form

--- myvenv/Scripts/mysite/urls.py
+++ myvenv/Scripts/mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'', include('blog.urls')),
+	url(r'^$', views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
 ]
